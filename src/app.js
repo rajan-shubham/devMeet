b8c6a4510009f38da8fbcd2b0b4652745e4dff95
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-14 19:47:01 +0530

    get,post,delete routig&requests

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const app = express(); // new(web server) expressjs application
 
-app.use("/dashboard", (req, res) => {
-    res.send("Server Dashboard");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Shubham", lastName: "Rajan" });
 });
 
-app.use("/hello", (req, res) => {
+app.post("/user", (req, res) => { 
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data Deleted Successfully");
+});
+
+// this will match all the HTTP method calls to /test
+app.use("/test", (req, res) => { 
     res.send("Hello from SERVER");
-});
-
-app.use("/", (req, res) => {
-    res.send("Hello from Rajan!");
 });
 
 app.listen(1234, () => {
