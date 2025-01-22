c35a8d69bcfdc51a6542f553ffcc06373eb3c3e7
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-22 16:46:13 +0530

    next() in req hadeler, taking param from link/path

--- src/app.js
+++ src/app.js
@@ -2,24 +2,17 @@ const express = require('express');
 
 const app = express(); // new(web server) expressjs application
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Shubham", lastName: "Rajan" });
-});
-
-app.post("/user", (req, res) => { 
-    // saving data to DB
-    res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Data Deleted Successfully");
-});
-
-// this will match all the HTTP method calls to /test
-app.use("/test", (req, res) => { 
-    res.send("Hello from SERVER");
-});
+app.use("/user",
+    (req, res, next) => { 
+        console.log("Hadeling the route");
+        // res.send("Response!!");
+        next();
+    },
+    (req, res) => {
+        console.log("Handeling the route 2!!");
+        res.send("2nd Response!!");
+    }
+);
 
 app.listen(1234, () => {
     console.log("Server is successfully listening http://localhost:1234");
