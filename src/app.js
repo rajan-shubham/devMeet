2d60aba5dede9113641e116e51a2bd09ffd10c6f
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-23 23:47:21 +0530

    multiple route handelers

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ const express = require('express');
 const app = express(); // new(web server) expressjs application
 
 app.use("/user",
+    [
     (req, res, next) => { 
-        console.log("Hadeling the route");
-        // res.send("Response!!");
+        console.log("Hadeling the route 1");
+        next();
+        // res.send("Response!!"); 
+    },
+    (req, res, next) => {
+        console.log("Handeling the route 2!!");
+        // res.send("2nd Response!!");
         next();
     },
     (req, res) => {
-        console.log("Handeling the route 2!!");
-        res.send("2nd Response!!");
+        console.log("Handeling the route 3!!");
+        res.send("3rd Response!!");
     }
+]
 );
 
 app.listen(1234, () => {
