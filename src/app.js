5cf7afa6422cfa8d95451ea867f648e608b0158e
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-24 22:54:29 +0530

    middlewares and req.handelers

--- src/app.js
+++ src/app.js
@@ -2,23 +2,21 @@ const express = require('express');
 
 const app = express(); // new(web server) expressjs application
 
-app.use("/user",
-    [
-    (req, res, next) => { 
-        console.log("Hadeling the route 1");
+// GET /users => middleware chain => request handler
+
+app.use("/", (req, res, next) => {
+    // res.send("Handeling / route"); 
+    next();
+});
+
+app.get("/user",
+    (req, res, next) => {
+        console.log("Handeling /user route"); 
         next();
-        // res.send("Response!!"); 
     },
     (req, res, next) => {
-        console.log("Handeling the route 2!!");
-        // res.send("2nd Response!!");
-        next();
-    },
-    (req, res) => {
-        console.log("Handeling the route 3!!");
-        res.send("3rd Response!!");
+        res.send("1st Route Handeler");
     }
-]
 );
 
 app.listen(1234, () => {
