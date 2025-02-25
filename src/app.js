abfd16c915590d0e71bfead838c42d3c892445d5
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-02-26 00:32:03 +0530

    creating user model, postApi, saving userobj to user collection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const connectDB = require("./config/database");
+const User = require('./models/user');
 const app = express(); // new(web server) expressjs application
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "sonali",
+        lastName: "kumari",
+        emailId: "axplusby",
+        password: "pass",
+        age: 18,
+        gender: "female",
+    }
+    const user = new User(userObj);
+    try {
+        await user.save();
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(400).send("Error in saving the user:" + err.message);
+    }
+});
 
 connectDB().then(() => {
     console.log("Database connected successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,27 @@
 // what a user in our database is
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
