dab1f7b24705468636df4d757030ad01138c3c79
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-05-19 00:40:00 +0530

    update: signup

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -21,8 +21,10 @@ router.post("/signup", async (req, res) => {
             password: passwordHash,
         });
 
-        await user.save();
-        res.send("User added successfully");
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+        res.cookie("tokenShubham", token, {expires: new Date(Date.now() + 8 * 3600000)})
+        res.json({message : "User added successfully", data : savedUser});
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
